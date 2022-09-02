762b76a9e9fa867267e0e99c86be491c9ce0645a
poommin2543 <poommin2543@gmail.com>
2022-09-02 21:48:43 +0700

    Numerical Differentiation Backword

---
+++ week3/Numerical Differentiation Backword.py
@@ -0,0 +1,17 @@
+#backword
+#diff 1 step
+def f(x):
+    return -0.1*x**4 - 0.15*x**3 - 0.5*x**2 - 0.25*x + 1.2
+
+x = 0.5 
+h = 0.25
+
+fx = (f(x)-f(x-h))/h
+
+print(fx)
+
+#diff 2 step
+print(f(0))
+
+fxx = (f(x)-2*f(x-h)+f(x-h-h))/(h**2)
+print(fxx)
