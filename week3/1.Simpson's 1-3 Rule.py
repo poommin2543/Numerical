8133a4d09f9037b3b0ecbfe1107dc092364a4455
poommin2543 <poommin2543@gmail.com>
2022-09-02 16:34:33 +0700

    Simpson's 1-3 Rule and plot

--- week3/1.Simpson's 1-3 Rule.py
+++ week3/1.Simpson's 1-3 Rule.py
@@ -2,37 +2,34 @@ import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 trueValue = 1.640533
-n = 5
 a = 0
 b = 0.8
+n = 10
 n = n+1
 listTable = []
 listError = []
 listN = []
-
 for n in range(2,n+1):
     # print(n)
-    if (n-1==0):
-        h = 0
-    else:
-        h = (b - a) / (n-1)
+    h = (b - a) / (n - 1)
     x = np.linspace(a, b, n)
-    #function
+    # print(x)
     f = 0.2+25*x-200*x**2+675*x**3-900*x**4+400*x**5
     # print(f)
-    I_trap = (h/2)*(f[0] + 2 * sum(f[1:n-1]) + f[n-1])
-    err_trap = (np.abs(I_trap-trueValue)/I_trap)*100
+    # print(f[1:n-1:2])
+    I_simp = (h/3) * (f[0] + 2*sum(f[2:n:2])+ 4*sum(f[1:n-1:2]) + f[n-1])
+    err_simp = (np.abs(I_simp-trueValue)/I_simp)*100
     listN.append(n-1)
-    listError.append(err_trap)
-    listTable.append([n-1,f'{h:.4f}',f'{I_trap:.4f}',f'{err_trap:.4f}'])
-    # print(I_trap)
-    # print(err_trap)
-    # print(f'n:{n-1}\th:{h:.4f}\tI:{I_trap:.4f}\terr:{err_trap:.4f}')
+    listError.append(err_simp)
+    listTable.append([n-1,f'{h:.4f}',f'{I_simp:.4f}',f'{err_simp:.4f}'])
+
+    # print(h)
 
 #printTable
 headers=["N", "H", "I_trap", "err_trap"]
 print(pandas.DataFrame(listTable,columns=headers))
 
+# print(listError)
 #plots
 plt.plot(listN,listError,color='red')
 plt.ylabel('%Error')
