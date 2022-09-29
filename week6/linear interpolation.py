48c89f683581ac40ae0e205efe97960dc3c47ee6
poommin2543 <poommin2543@gmail.com>
2022-09-29 12:53:32 +0700

    Linear interpolation muti

--- week6/linear interpolation.py
+++ week6/linear interpolation.py
@@ -31,3 +31,20 @@ print("Answer {} is".format(Goal),interpolation(data, Goal))
 error = np.abs(ln(Goal)-interpolation(data, Goal))/ln(Goal)*100
 print(error)
 
+#multi
+
+# Implementation of Linear Interpolation using Python3 code
+# Importing library
+
+# from scipy.interpolate import interp1d
+ 
+# X = [1,2,3,4,5] # random x values
+# Y = [11,2.2,3.5,-88,1] # random y values
+ 
+# # test value
+# interpolate_x = 2.5
+ 
+# # Finding the interpolation
+# y_interp = interp1d(X, Y)
+# print("Value of Y at x = {} is".format(interpolate_x),
+#       y_interp(interpolate_x))
